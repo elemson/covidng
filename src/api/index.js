38912beec22b360028c202fdc58689b2881fc339
elemson <toluwhani@gmail.com>
2020-04-21 15:27:27 +0300

    project setup and folder/file structure

---
+++ src/api/index.js
@@ -0,0 +1,10 @@
+import axios from "axios";
+const url = "https://covid19.nathdro.id/api";
+
+const fetchData = async () => {
+  try {
+    const response = await axios.get(url);
+
+    return response;
+  } catch (error) {}
+};
